239a801cdc11d012acc4e57dd212c0f7a3972268
Adam Casey <adam.jw.casey@gmail.com>
2023-10-28 11:46:43 -0600

    Runs 'hello world' correctly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,77 @@
+#![warn(clippy::pedantic)]
+
+use std::env;
+use std::fs;
+use std::collections::HashMap;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    let bytes = contents.as_bytes();
+
+    let mut data = HashMap::<i32, u8>::new();
+    let mut instruction_pointer = 0;
+    let mut data_pointer = 0;
+
+    while instruction_pointer < bytes.len(){
+        match bytes[instruction_pointer]{
+            b'[' => {
+                if *data.entry(data_pointer).or_default() == 0{
+                    let mut depth = 0;
+                    while instruction_pointer < bytes.len(){
+                        match bytes[instruction_pointer]{
+                            b'[' => depth += 1,
+                            b']' => {
+                                depth -= 1;
+                                if depth == 0{
+                                    break;
+                                }
+                            },
+                            _ => (),
+                        };
+                        instruction_pointer += 1;
+                    }
+                }else{
+                    instruction_pointer += 1;
+                }
+            },
+            b']' => {
+                #[allow(clippy::if_not_else)]
+                if *data.entry(data_pointer).or_default() != 0{
+                    let mut depth = 0;
+                    while instruction_pointer < bytes.len(){
+                        match bytes[instruction_pointer]{
+                            b']' => depth += 1,
+                            b'[' => {
+                                depth -= 1;
+                                if depth == 0{
+                                    break;
+                                }
+                            },
+                            _ => (),
+                        };
+                        instruction_pointer -= 1;
+                    }
+                }else{
+                    instruction_pointer += 1;
+                }
+            },
+            other => {
+                match other{
+                    b'<' => data_pointer -= 1,
+                    b'>' => data_pointer += 1,
+                    b'+' => *data.entry(data_pointer).or_default() += 1,
+                    b'-' => *data.entry(data_pointer).or_default() -= 1,
+                    b'.' => print!("{}", *data.entry(data_pointer).or_default() as char),
+                    b',' => {std::io::stdin().read_exact(std::slice::from_mut(data.entry(data_pointer).or_default())).expect("This should work");},
+                    _ => (),
+                };
+                instruction_pointer += 1;
+            }
+        }
+    }
 }
